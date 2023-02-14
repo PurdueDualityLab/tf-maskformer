3ea18d2005a874ff990a141475a27b37bafa575d
Ayush Praharaj <ayushprjaharaj@gmail.com>
2023-02-14 12:35:02 -0500

    Finished first iteration of Implementation (NOT TESTED)

--- official/projects/modeling/layers/nn_block.py
+++ official/projects/modeling/layers/nn_block.py
@@ -1,43 +1,48 @@
 import tensorflow as tf
 
 '''
+Transformer Parameters:
+
 enc_layers: int,
 dec_layers: int,
 nheads: int,
 dropout: float,
 dim_feedforward: int,
 pre_norm: bool,
+enforce_input_project: bool
 '''
 
 class MaskFormerPredictor(tf.keras.layers.Layer):
     def __init__(self,
                  num_classes: int,
                  hidden_dim: int,
-                 deep_supervision: bool,
-                 mask_dim: int,
-                 enforce_input_project: bool):
+                #  dec_supervision: bool,
+                 mask_dim: int):
         super().__init__()
         self.mlp = MLP(hidden_dim, hidden_dim, mask_dim, 3)
-        self.linear_classifier = tf.keras.layers.Dense(input_shape=hidden_dim, out_dim=num_classes + 1, activation=None)
+        self.linear_classifier = tf.keras.layers.Dense(input_shape=hidden_dim, out_dim=num_classes + 1, activation=tf.keras.activations.softmax)
         # No Softmax used in their code? Need to figure out!!
-            
-
-    # def build(self, input_shape):
-    #     self._linear_classifier = None
+        # self.linear_classifier = tf.keras.layers.Dense(input_shape=hidden_dim, out_dim=num_classes + 1, activation=None)
         
-    # dot product
-    def get_mask_predictions(self):
-        # Have to figure out where they use per_pixel_embeddings
-        return None
+        # self.dec_supervision = dec_supervision
+            
+    '''
+    def build(self, input_shape):
+        self._linear_classifier = None
+        
+    def get_mask_predictions(self, per_pixel_embeddings, mask_embeddings):
+        if(self.dec_supervision):
+            outputs_seg_masks = tf.einsum("lbqc,bchw->lbqhw", mask_embed, mask_features)
+    '''
 
     def call(self, inputs):
         per_pixel_embeddings = inputs['per_pixel_embeddings']
         per_segment_embeddings = inputs['per_segment_embeddings']
 
         class_prob_prediction = self.linear_classifier(per_segment_embeddings)
-
         mask_embedding = self.mlp(per_segment_embeddings)
-        mask_prob_prediction = self.get_mask_predictions(per_pixel_embeddings, mask_embedding)
+        mask_prob_prediction = tf.einsum("lbqc,bchw->lbqhw", per_pixel_embeddings, mask_embedding)
+        # Might need to change first arg in einsum after finding shapes
 
         return {'class_prob_predictions': class_prob_prediction,'mask_prob_predictions': mask_prob_prediction}
         
