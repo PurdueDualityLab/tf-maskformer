ff60168f057f21fa6939a5d80468b128ed97ad81
Alexiy Buynitsky <abuynits@gmail.com>
2023-02-19 00:08:09 -0500

    feat:updated pl script

--- official/projects/dataloaders/coco_to_tf_records.pl
+++ official/projects/dataloaders/coco_to_tf_records.pl
@@ -27,13 +27,32 @@ my $train_coco_url = "http://images.cocodataset.org/zips/train2017.zip";
 my $val_coco_url = "http://images.cocodataset.org/zips/val2017.zip";
 my $annotations_coco_url = "http://images.cocodataset.org/annotations/panoptic_annotations_trainval2017.zip";
 # download the file paths
-system("wget $val_coco_url -p $coco_file_path");
-system("wget $train_coco_url -p $coco_file_path");
-system("wget $annotations_coco_url -p $coco_file_path");
+unless (-d $tfrecord_file_path) {
+  print "$tfrecord_file_path does not exist!\n";
+  exit 1;
+}
 
-# unzip the file paths
-system("unzip $coco_file_path/train2017.zip &> /dev/null");
-system("unzip $coco_file_path/val2017.zip &> /dev/null");
-system("unzip $coco_file_path/panoptic_annotations_trainval2017.zip &> /dev/null");
+unless (-d "$coco_file_path/train2017.zip") {
+  print "downloading $coco_file_path/train2017.zip\n";
+  system("wget $train_coco_url -P $coco_file_path");
+}else{
+  print "$coco_file_path/train2017.zip already exists!\n";
+}
+unless (-d "$coco_file_path/val2017.zip") {
+  print "downloading $coco_file_path/val2017.zip\n";
+  system("wget $val_coco_url -P $coco_file_path");
+}else{
+  print "$coco_file_path/val2017.zip already exists!\n";
+}
+unless (-d "$coco_file_path/panoptic_annotations_trainval2017.zip") {
+  print "downloading $coco_file_path/panoptic_annotations_trainval2017.zip\n";
+  system("wget $annotations_coco_url -P $coco_file_path");
+}else{
+print "$coco_file_path/panoptic_annotations_trainval2017.zip already exists!\n";
+}
+
+system("unzip $coco_file_path/train2017.zip -d $coco_file_path");
+system("unzip $coco_file_path/val2017.zip -d $coco_file_path");
+system("unzip $coco_file_path/panoptic_annotations_trainval2017.zip -d $coco_file_path");
 
 system("python $build_coco_path --coco_root=$coco_file_path --output_dir=$tfrecord_file_path");
