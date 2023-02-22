6328a5c76432e643d2a196d49b93b2bcc48a2707
Ananya Singh <xananya.singhx@gmail.com>
2023-02-22 10:59:29 -0500

    added classification loss

--- official/projects/losses/maskformer_losses.py
+++ official/projects/losses/maskformer_losses.py
@@ -37,16 +37,61 @@ class DiceLoss(tf.keras.losses.Loss):
     def __init__(self):
         pass
 
-class ClassificationLoss():
-    def call(self, outputs, y_true, indices, num_masks):
-        pass
+# verify concat and fill
+# find out how to set device and if its necessary line 96 of pytorch
 
-class MaskLoss():
-    def call(self, outputs, y_true, indices, num_masks):
-        pass
+class Loss():
+    def __init__():
+        self.num_classes = 
+        self.empty_weight = 
 
-# focal = FocalLoss()
-# output_shape = [8, 100, 160, 160]
-# tf_pred = tf.random.uniform(output_shape)
-# tf_true = tf.random.uniform(output_shape,0,1)
-# print(focal.call(tf_true, tf_pred,8))
+    def _get_pred_permutation_idx(self, indices):
+        batch_idx = tf.concat([tf.fill(pred,i) for i, (pred,_) in enumerate(indices)], axis=0)
+        pred_idx = tf.concat([pred for (pred,) in indices], axis=0)
+        return batch_idx, pred_idx
+    
+    def _get_true_permutation_idx(self, indices):
+        batch_idx = tf.concat([tf.fill(true,i) for i, (_,true) in enumerate(indices)], axis=0)
+        true_idx = tf.concat([true for (_,true) in indices], axis=0)
+        return batch_idx, true_idx
+
+class ClassificationLoss(Loss):
+    def call(self, outputs, y_true, indices, num_masks):
+        assert "pred_logits" in outputs
+
+        pred_logits = outputs["pred_logits"]
+
+        idx = super()._get_pred_permutation_idx(indices)
+        true_classes_o = tf.concat([t["labels"][J] for t, (_, J) in zip(y_true, indices)], axis=0)
+        true_classes = tf.cast(tf.fill(pred_logits.shape[:2], super().num_classes), dtype=tf.int64) # device?
+        # target_classes = tf.tensor_scatter_nd_update(target_classes, tf.expand_dims(idx, axis=1), target_classes_o)
+        true_classes[idx] = true_classes_o
+
+        # loss_ce = tf.nn.softmax_cross_entropy_with_logits(y_true, tf.transpose(pred_logits,(1,2)))
+        loss_ce = tf.nn.weighted_cross_entropy_with_logits(y_true, tf.transpose(pred_logits,(1,2)), super().empty_weight)
+        losses = {"loss_ce": loss_ce}
+        return losses
+
+class MaskLoss(Loss):
+    def call(self, outputs, y_true, indices, num_masks):
+        assert "pred_masks" in outputs
+
+        pred_idx = super()._get_pred_permutation_idx(indices)
+        true_idx = super()._get_true_permutation_idx(indices)
+        pred_masks = outputs["pred_masks"]
+        pred_masks = pred_masks[pred_idx]
+        masks = [t["masks"] for t in y_true]
+        # true_masks, valid = 
+        # true_masks = 
+        # true_masks = 
+
+        
+
+        pred_masks = tf.reshape(pred_masks[:, 0], -1)
+        true_masks = tf.reshape(true_masks, -1)
+        true_masks = tf.reshape(true_masks, pred_masks.shape)
+        losses = {
+            "loss_mask": FocalLoss().call(pred_masks, true_masks, num_masks),
+            "loss_dice": DiceLoss().call(pred_masks, true_masks, num_masks)
+        }
+        return losses
