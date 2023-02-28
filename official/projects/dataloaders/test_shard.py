b46b10cb6c5671b210ce4f926c3a1e50b45aa9c7
Alexiy Buynitsky <abuynits@gilbreth-fe02.rcac.purdue.edu>
2023-02-28 09:51:55 -0500

    feat:cleaned up tester repo

--- official/projects/dataloaders/test_shard.py
+++ official/projects/dataloaders/test_shard.py
@@ -7,40 +7,36 @@ from panoptic_input import mask_former_parser
 from PIL import Image
 import numpy as np
 import cv2
+
 parser_fn = mask_former_parser([1024,1024])
+file_path = "/scratch/gilbreth/abuynits/coco_ds/tfrecords/val-00000-of-00008.tfrecord" # specify the filepath to tfrecord
+save_im_path = "/home/abuynits/projects/tf-maskformer/official/projects/dataloaders/img.png" # image save path for displaying image
+# returns de-normalized tensor
+def get_un_normalized_np(im_tensor):
+    np_data = im_tensor.numpy()
+    print(np_data)
+    print(np_data.shape)
 
-
-def display_im(feat):
-    for key in feat.keys():
-        if key != "image":
-            print(f"{key}: {feat[key]}")
-
-    print(f"Image shape: {feat['image'].shape}")
-    
-    plt.figure(figsize=(7, 7))
-    plt.imshow(feat["image"].numpy())
-    plt.show()
-def display_pil_im(np_data):
     norm_image = cv2.normalize(np_data, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
     norm_image = norm_image.astype(np.uint8)
-    im = Image.fromarray(norm_image, 'RGB')
-    im.save("/home/abuynits/projects/tf-maskformer/official/projects/dataloaders/img.png")
-file_path = "/scratch/gilbreth/abuynits/coco_ds/tfrecords/val-00000-of-00008.tfrecord"
+    return norm_image
+    
+# displays an image
+def display_pil_im(np_data):
+    print(np_data)
+    print(np_data.shape)
+    im = Image.fromarray(np_data, 'RGB')
+    im.save(save_im_path)
+
 raw_dataset = tf.data.TFRecordDataset(file_path)
 for raw_record in raw_dataset.take(1):
     example = tf.train.Example()
     example.ParseFromString(raw_record.numpy())
-    #print(raw_record)
     parsed_record = parser_fn(raw_record)
     print("==================\n\n\n")
     print(parsed_record[1]) # prints image dictionary holding all mask info
-    print(type(parsed_record[0])) # print type of image input to model
-    print(type(parsed_record[1]))
-    np_data = parsed_record[0].numpy()
-    print(np_data)
-    print(np_data.shape)
-    print(parsed_record[1].keys())
-    display_pil_im(np_data)
+    print(parsed_record[1].keys()) # prints available dictionary info keys
+    display_pil_im(get_un_normalized_np(parsed_record[0])) # displays the pil image
 
 
 
