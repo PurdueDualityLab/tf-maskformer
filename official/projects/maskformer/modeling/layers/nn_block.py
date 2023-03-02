2b1a064b6cb9809110f3815c250e1af74862feb8
IbrahimSaeedPurdue <54752803+IbrahimSaeedPurdue@users.noreply.github.com>
2023-03-01 23:21:41 -0500

    Wrote MLPHead tests

--- official/projects/maskformer/modeling/layers/nn_block.py
+++ official/projects/maskformer/modeling/layers/nn_block.py
@@ -12,53 +12,69 @@ pre_norm: bool,
 enforce_input_project: bool
 '''
 
-class MaskFormerPredictor(tf.keras.layers.Layer):
+
+class MLPHead(tf.keras.layers.Layer):
     def __init__(self,
                  num_classes: int,
                  hidden_dim: int,
-                #  dec_supervision: bool,
+                 #  dec_supervision: bool,
                  mask_dim: int):
         super().__init__()
-        self.mlp = MLP(hidden_dim, hidden_dim, mask_dim, 3)
-        self.linear_classifier = tf.keras.layers.Dense(input_shape=hidden_dim, out_dim=num_classes + 1, activation=tf.keras.activations.softmax)
+
+        self._num_classes = num_classes
+        self._hidden_dim = hidden_dim
+        self._mask_dim = mask_dim
+
+    def build(self, input_shape):
+        self._mlp = MLP(self._hidden_dim, self._hidden_dim, self._mask_dim, 3)
+        self._linear_classifier = tf.keras.layers.Dense(self._num_classes + 1)
         # No Softmax used in their code? Need to figure out!!
         # self.linear_classifier = tf.keras.layers.Dense(input_shape=hidden_dim, out_dim=num_classes + 1, activation=None)
-        
+
         # self.dec_supervision = dec_supervision
-            
-def call(self, inputs):
+
+    def call(self, inputs):
         per_pixel_embeddings = inputs['per_pixel_embeddings']
         per_segment_embeddings = inputs['per_segment_embeddings']
 
-        class_prob_prediction = self.linear_classifier(per_segment_embeddings)
-        mask_embedding = self.mlp(per_segment_embeddings[-1])
-        mask_prob_prediction = tf.einsum("bqc,bchw->bqhw", mask_embedding, per_pixel_embeddings)
+        class_prob_prediction = self._linear_classifier(per_segment_embeddings)
+        mask_embedding = self._mlp(per_segment_embeddings[-1])
+        mask_prob_prediction = tf.einsum(
+            "bqc,bchw->bqhw", mask_embedding, per_pixel_embeddings)
 
-        return class_prob_prediction, mask_prob_prediction
-        
+        return {'class_prob_predictions': class_prob_prediction[-1],'mask_prob_predictions': mask_prob_prediction}
+
 
 class MLP(tf.keras.layers.Layer):
     def __init__(self,
-                input_dim: int, 
-                hidden_dim: int, 
-                output_dim: int, 
-                num_layers: int):
+                 input_dim: int,
+                 hidden_dim: int,
+                 output_dim: int,
+                 num_layers: int):
         super().__init__()
-        self.num_layers = num_layers
-        layer_dims = [(input_dim, hidden_dim)]
-        for _ in range(num_layers - 2):
-            layer_dims.append((hidden_dim, hidden_dim))
-        layer_dims.append((hidden_dim, output_dim))
-        #layer_dims contains the input and output dimension of each layer in the form (input dimension, output dimension)
-        self.layers = []
-        for i, dim in enumerate(layer_dims):
-            if(i < num_layers - 1):
-                self.layers.append(tf.keras.layers.Dense(dim[1], activation=tf.nn.relu))
-            else:
-                #Final Layer
-                self.layers.append(tf.keras.layers.Dense(dim[1], activation=None))
 
-    def call(self, x): 
-        for layer in self.layers:
+        self._input_dim = input_dim
+        self._hidden_dim = hidden_dim
+        self._output_dim = output_dim
+        self._num_layers = num_layers
+
+    def build(self, input_shape):
+        layer_dims = [(self._input_dim, self._hidden_dim)]
+        for _ in range(self._num_layers - 2):
+            layer_dims.append((self._hidden_dim, self._hidden_dim))
+        layer_dims.append((self._hidden_dim, self._output_dim))
+
+        self._layers = []
+        for i, dim in enumerate(layer_dims):
+            if(i < self._num_layers - 1):
+                self._layers.append(tf.keras.layers.Dense(
+                    dim[1], activation=tf.nn.relu))
+            else:
+                # Final Layer
+                self._layers.append(
+                    tf.keras.layers.Dense(dim[1], activation=None))
+
+    def call(self, x):
+        for layer in self._layers:
             x = layer(x)
         return x
