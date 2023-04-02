4ce6174d41969c18fbc231fb6a1212e8932d3e15
InAIBot <112273607+InAIBot@users.noreply.github.com>
2023-04-02 14:47:37 -0400

    Update maskformer.py

--- official/projects/maskformer/maskformer.py
+++ official/projects/maskformer/maskformer.py
@@ -9,35 +9,90 @@ from official.projects.maskformer.modeling.layers.nn_block import MLPHead
 class Maskformer(tf.keras.Model):
   """Maskformer"""
   def __init__(self,
-              num_classes = 171,
-              batch_size = 1,
+               fpn_feat_dims=256,
+               data_format=None,
+               dilation_rate=(1, 1),
+               groups=1,
+               activation='relu',
+               use_bias=False,
+               kernel_initializer="glorot_uniform",
+               bias_initializer="zeros",
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               num_queries=100,
+               hidden_size=256,
+               num_encoder_layers=0,
+               num_decoder_layers=6,
+               dropout_rate=0.1,
+               backbone_endpoint_name='5',
+               num_classes=171,
+               batch_size=1,
                **kwargs):
-    self.num_classes = num_classes
-    self.batch_size = batch_size
+    self._batch_size = batch_size
+    self._num_classes = num_classes
+
+    # Pixel Deocder paramters.
+    self._fpn_feat_dims = fpn_feat_dims
+    self._data_format = data_format
+    self._dilation_rate = dilation_rate
+    self._groups = groups
+    self._activation = activation
+    self._use_bias = use_bias
+    self._kernel_initializer = kernel_initializer
+    self._bias_initializer = bias_initializer
+    self._kernel_regularizer = kernel_regularizer
+    self._bias_regularizer = bias_regularizer
+    self._activity_regularizer = activity_regularizer
+    self._kernel_constraint = kernel_constraint
+    self._bias_constraint = bias_constraint
+
+    # DETRTransformer parameters.
+    self._num_encoder_layers = num_encoder_layers
+    self._num_decoder_layers = num_decoder_layers
+    self._num_queries = num_queries
+    self._hidden_size = hidden_size
+    self._dropout_rate = dropout_rate
+    self._backbone_endpoint = backbone_endpoint_name
+
+
     super(Maskformer, self).__init__(**kwargs)
 
   def build(self, image):
-    self.batch_size = self.batch_size
     #backbone
     self.backbone = resnet.ResNet(50, bn_trainable=False)
     #decoders
-    self.pixel_decoder = Fpn()
-    self.transformer = MaskFormerTransformer(backbone_endpoint_name='5',
-                                            batch_size=self.batch_size,
-                                            num_queries=100,
-                                            hidden_size=256,
+    self.pixel_decoder = Fpn(fpn_feat_dims=self._fpn_feat_dims,
+                            data_format=self._data_format,
+                            dilation_rate=self._dilation_rate,
+                            groups=self._groups,
+                            activation=self._activation,
+                            use_bias=self._use_bias,
+                            kernel_initializer=self._kernel_initializer,
+                            bias_initializer=self._bias_initializer,
+                            kernel_regularizer=self._kernel_regularizer,
+                            bias_regularizer=self._bias_regularizer,
+                            activity_regularizer=self._activity_regularizer,
+                            kernel_constraint=self._kernel_constraint,
+                            bias_constraint=self._bias_constraint)
+    self.transformer = MaskFormerTransformer(backbone_endpoint_name=self._backbone_endpoint,
+                                            batch_size=self._batch_size,
+                                            num_queries=self._num_queries,
+                                            hidden_size=self._hidden_size,
                                             num_classes=self.num_classes,
-                                            num_encoder_layers=0,
-                                            num_decoder_layers=6,
-                                            dropout_rate=0.1)
+                                            num_encoder_layers=self.num_encoder_layers,
+                                            num_decoder_layers=self._num_decoder_layers,
+                                            dropout_rate=self._dropout_rate)
     #Heads
     self.pixel_predictor = tf.keras.layers.Conv2D(filters=self.num_classes,
                                                   strides=(1, 1),
                                                   kernel_size=(1, 1),
                                                   padding='valid')
-    self.head = MLPHead(num_classes = self.num_classes, 
-                                    hidden_dim = 256, 
-                                    mask_dim = 256)
+    self.head = MLPHead(num_classes=self.num_classes, 
+                        hidden_dim=self._hidden_size, 
+                        mask_dim=self._fpn_feat_dims)
    
     super(Maskformer, self).build(image)
  
