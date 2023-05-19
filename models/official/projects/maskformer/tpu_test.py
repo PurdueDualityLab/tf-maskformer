88116ab1cd7d9b977cdba7f2e3408b49bb009329
Vishal-S-P <vishal_purohit@outlook.com>
2023-05-19 05:27:57 -0400

    working tpu test

--- models/official/projects/maskformer/tpu_test.py
+++ models/official/projects/maskformer/tpu_test.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 if __name__ == "__main__":
     cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-      tpu="tf-debug-2", project="red-atlas-305317", zone="us-central1-a")
+      tpu="tf-train-2", project="red-atlas-305317", zone="europe-west4-a")
     tf.config.experimental_connect_to_cluster(cluster_resolver)
     tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
     strategy = tf.distribute.TPUStrategy(cluster_resolver)
