e17026eec9399d77316e2bf1abd419ff6114fbf1
Vishal-S-P <vishal_purohit@outlook.com>
2023-05-19 05:33:10 -0400

    updated zone and project id for TPU

--- models/official/common/distribute_utils.py
+++ models/official/common/distribute_utils.py
@@ -82,8 +82,9 @@ def tpu_initialize(tpu_address):
   Returns:
     A TPUClusterResolver.
   """
+  
   cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(
-      tpu=tpu_address)
+      tpu=tpu_address, project=os.environ["TPU_PROJECT"], zone=os.environ["TPU_ZONE"])
   if tpu_address not in ("", "local"):
     tf.config.experimental_connect_to_cluster(cluster_resolver)
   tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
