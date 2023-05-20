3bfc6395a949bb34e45f333f1232fdebe2f2b22a
Vishal-S-P <vishal_purohit@outlook.com>
2023-05-20 07:57:46 -0400

    update

--- models/official/projects/maskformer/tasks/panoptic_maskformer.py
+++ models/official/projects/maskformer/tasks/panoptic_maskformer.py
@@ -33,7 +33,7 @@ class PanopticTask(base_task.Task):
                                  num_encoder_layers=0,
                                  num_decoder_layers=6,
                                  num_classes=199,
-                                 batch_size=32)
+                                 batch_size=1)
 
 		return model
 	
