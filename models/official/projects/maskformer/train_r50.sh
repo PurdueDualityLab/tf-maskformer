2af95504ff0bd975c12fc819436567e8811a2da5
Vishal-S-P <vishal_purohit@outlook.com>
2023-06-17 13:06:03 -0400

    update

--- models/official/projects/maskformer/train_r50.sh
+++ models/official/projects/maskformer/train_r50.sh
@@ -6,7 +6,7 @@ gcsfuse cam2-models ~/models
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export MODEL_DIR="gs://cam2-models/maskformer"
 export DATA_PTH="gs://cam2-datasets/coco_panoptic"
-export TPU_NAME="tf-debug-3"
+export TPU_NAME="tf-debug-4"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
 export TPU_ZONE="us-central1-a"
