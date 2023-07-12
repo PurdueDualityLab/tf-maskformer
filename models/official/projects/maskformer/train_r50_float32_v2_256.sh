3279bb9bdd646df7319f890f986f8a9919046ab5
Vishal-S-P <vishal_purohit@outlook.com>
2023-07-12 08:52:41 -0400

    added sh file for v2-256 training

--- models/official/projects/maskformer/train_r50_float32_v2_256.sh
+++ models/official/projects/maskformer/train_r50_float32_v2_256.sh
@@ -4,14 +4,14 @@ fusermount -u ~/models
 gcsfuse --implicit-dirs cam2-datasets ~/datasets
 gcsfuse cam2-models ~/models
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
-export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP_01_v256"
+export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP01_v256"
 export BACKBONE_DIR="gs://cam2-models/maskformer_dummy/resnet50_v1"
 export TPU_NAME="tf-train-1"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
 export TPU_ZONE="us-central1-a"
 export OVERRIDES="runtime.distribution_strategy=tpu,\
-task.train_data.global_batch_size=128,\
+task.train_data.global_batch_size=256,\
 task.train_data.cache=true"
 python3 models/official/projects/maskformer/train.py \
   --experiment maskformer_coco_panoptic \
