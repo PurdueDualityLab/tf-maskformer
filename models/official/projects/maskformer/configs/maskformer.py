2be40031fe36f99bc6fa58587f60b172e7b08438
Vishal-S-P <vishal_purohit@outlook.com>
2023-07-14 02:02:51 -0400

    changed the number of classes to 199 as we are using new tfrecords and reverted some of code sniipedt back told previous working commit

--- models/official/projects/maskformer/configs/maskformer.py
+++ models/official/projects/maskformer/configs/maskformer.py
@@ -115,7 +115,7 @@ class MaskFormerTask(cfg.TaskConfig):
 COCO_INPUT_PATH_BASE = 'gs://cam2-datasets/coco_panoptic/'
 COCO_TRAIN_EXAMPLES = 118287
 COCO_VAL_EXAMPLES = 5000
-SET_MODEL_BFLOAT16 = True
+SET_MODEL_BFLOAT16 = False
 SET_DATA_BFLOAT16 = True
 
 @exp_factory.register_config_factory('maskformer_coco_panoptic')
