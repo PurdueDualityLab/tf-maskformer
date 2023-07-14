5489347a77c3611d349114d3240a2ddcf393e9bd
Vishal-S-P <vishal_purohit@outlook.com>
2023-07-14 00:31:38 -0400

    updated sh for bfloat16 model

--- models/official/projects/maskformer/train_r50_bfloat16.sh
+++ models/official/projects/maskformer/train_r50_bfloat16.sh
@@ -4,13 +4,14 @@ fusermount -u ~/models
 gcsfuse --implicit-dirs cam2-datasets ~/datasets
 gcsfuse cam2-models ~/models
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
-export MODEL_DIR="gs://cam2-models/maskformer_tpu_profiling_bfloat16/v2"
+export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP_02_BFLOAT"
 export BACKBONE_DIR="gs://cam2-models/maskformer_dummy/resnet50_v1"
-export TPU_NAME="tf-debug-3"
+export TPU_NAME="tf-debug-2"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
 export TPU_ZONE="us-central1-a"
-export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=bfloat16,"
+export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=mixed_bfloat16,\
+task.train_data.global_batch_size=128,"
 python3 models/official/projects/maskformer/train.py \
   --experiment maskformer_coco_panoptic \
   --mode train \
