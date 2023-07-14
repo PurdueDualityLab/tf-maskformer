1cf34f7ba56421e6ad52077cdf2ecbe51db96f82
Vishal-S-P <vishal_purohit@outlook.com>
2023-07-14 00:34:47 -0400

    updated sh for bfloat16 model

--- models/official/projects/maskformer/train_r50_bfloat16.sh
+++ models/official/projects/maskformer/train_r50_bfloat16.sh
@@ -10,7 +10,7 @@ export TPU_NAME="tf-debug-3"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
 export TPU_ZONE="us-central1-a"
-export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=mixed_bfloat16,\
+export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=bfloat16,\
 task.train_data.global_batch_size=128,"
 python3 models/official/projects/maskformer/train.py \
   --experiment maskformer_coco_panoptic \
