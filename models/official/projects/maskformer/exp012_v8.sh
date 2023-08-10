88a736deddcafde088016f98362f1b78fd9147dd
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-10 06:17:20 -0400

    updated code for image size 640

--- models/official/projects/maskformer/exp012_v8.sh
+++ models/official/projects/maskformer/exp012_v8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-train_bsize=16
+train_bsize=64
 eval_bsize=16
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
@@ -12,7 +12,7 @@ export TFRECORDS_DIR="gs://cam2-datasets/coco_panoptic/tfrecords"
 export TRAIN_BATCH_SIZE=$train_bsize
 export EVAL_BATCH_SIZE=$eval_bsize
 export BASE_LR=0.00005
-export IMG_SIZE=1024
+export IMG_SIZE=640
 export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=float32,\
 task.train_data.global_batch_size=$TRAIN_BATCH_SIZE,task.model.which_pixel_decoder=transformer_fpn"
 python3 models/official/projects/maskformer/train.py \
