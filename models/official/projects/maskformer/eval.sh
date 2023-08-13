1bb81668e61a9f7e07fbd0aa1cd575992ec98e98
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-13 02:11:48 -0400

    updated code

--- models/official/projects/maskformer/eval.sh
+++ models/official/projects/maskformer/eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-train_bsize=2
-eval_bsize=2
+train_bsize=8
+eval_bsize=8
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP12_v8"
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
