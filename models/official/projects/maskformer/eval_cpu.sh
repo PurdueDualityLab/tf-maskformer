1ce2c0dcb493c85d21afc9bc16edf1826fda9a9d
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-14 05:39:02 -0400

    updated code with ability to eval on cpu

--- models/official/projects/maskformer/eval_cpu.sh
+++ models/official/projects/maskformer/eval_cpu.sh
@@ -5,16 +5,16 @@ export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP12_v8_eval"
 export MASKFORMER_CKPT="gs://cam2-models/maskformer_vishal_exps/EXP12_v8/ckpt-77616"
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
-export TPU_NAME="tf-debug-10"
-export TPU_SOFTWARE="2.11.0"
-export TPU_PROJECT="red-atlas-305317"
-export TPU_ZONE="europe-west4-a"
+# export TPU_NAME="tf-debug-10"
+# export TPU_SOFTWARE="2.11.0"
+# export TPU_PROJECT="red-atlas-305317"
+# export TPU_ZONE="europe-west4-a"
 export TFRECORDS_DIR="gs://cam2-datasets/coco_panoptic/tfrecords"
 export TRAIN_BATCH_SIZE=$train_bsize
 export EVAL_BATCH_SIZE=$eval_bsize
 export BASE_LR=0.0001
 export IMG_SIZE=640
-export OVERRIDES="runtime.distribution_strategy=onedevice,runtime.mixed_precision_dtype=float32,\
+export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precision_dtype=float32,\
 task.validation_data.global_batch_size=$EVAL_BATCH_SIZE,task.model.which_pixel_decoder=transformer_fpn,\
 task.init_checkpoint_modules=all,\
 task.init_checkpoint=$MASKFORMER_CKPT"
@@ -22,5 +22,4 @@ python3 models/official/projects/maskformer/train.py \
   --experiment maskformer_coco_panoptic \
   --mode eval \
   --model_dir $MODEL_DIR \
-  --tpu $TPU_NAME \
   --params_override=$OVERRIDES
