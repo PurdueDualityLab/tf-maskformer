e7522aa5a4a3c238c7d89bf3342fe9e497d48c5f
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-15 02:20:52 -0400

    EXP013_v128 code

--- models/official/projects/maskformer/exp013_v128.sh
+++ models/official/projects/maskformer/exp013_v128.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-train_bsize=32
+train_bsize=128
 eval_bsize=16
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
 export MASKFORMER_CKPT="gs://cam2-models/maskformer_vishal_exps/EXP12_v8/ckpt-77616"
 export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP13_v128"
-export TPU_NAME="tf-debug-10"
+export TPU_NAME="tf-train-1"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
 export TPU_ZONE="europe-west4-a"
