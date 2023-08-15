3939c1164b38aaa8d65dc252a49b06a6bc9ea01c
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-15 06:10:48 -0400

    added train_and_eval option

--- models/official/projects/maskformer/configs/maskformer.py
+++ models/official/projects/maskformer/configs/maskformer.py
@@ -186,7 +186,7 @@ def maskformer_coco_panoptic() -> cfg.ExperimentConfig:
           steps_per_loop=steps_per_epoch,
           summary_interval=ckpt_interval,
           checkpoint_interval=ckpt_interval,
-          validation_interval=steps_per_epoch,
+          validation_interval=1,
           max_to_keep=3,
           best_checkpoint_export_subdir='best_ckpt',
           # TODO: Metric not implemented yet
