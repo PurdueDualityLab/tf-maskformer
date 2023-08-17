673ee9a029a303e368849598a8ff4b74b939ad41
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-17 02:46:10 -0400

    verifying all the tfrecords

--- models/official/projects/maskformer/dataloaders/test_shard.py
+++ models/official/projects/maskformer/dataloaders/test_shard.py
@@ -14,7 +14,7 @@ decoder = panoptic_input.TfExampleDecoder()
 for each_file in file_paths:
     raw_dataset = tf.data.TFRecordDataset(os.path.join(tfrecord_path, each_file))
     print("Reading file :", os.path.join(tfrecord_path, each_file))
-    for raw_record in raw_dataset.take(1):
+    for raw_record in raw_dataset.take():
         data = decoder.decode(raw_record)
         print("Decoded file :", os.path.join(tfrecord_path, each_file))
         image = data['image']
