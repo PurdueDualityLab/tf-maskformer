7a4794df1b67ce95e92d4bcdc15d36e67b68dbf5
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-17 03:35:21 -0400

    verifying all the tfrecords

--- models/official/projects/maskformer/dataloaders/test_shard.py
+++ models/official/projects/maskformer/dataloaders/test_shard.py
@@ -12,6 +12,8 @@ tfrecord_path = "/home/vishalpurohit55595/datasets/coco_panoptic/tfrecords"  # s
 file_paths = tf.io.gfile.glob(tfrecord_path + "/*.tfrecord")
 decoder = panoptic_input.TfExampleDecoder()
 image_count = 0
+h = []
+w = []
 for each_file in file_paths:
     raw_dataset = tf.data.TFRecordDataset(os.path.join(tfrecord_path, each_file))
     print("Reading file :", os.path.join(tfrecord_path, each_file))
@@ -19,9 +21,35 @@ for each_file in file_paths:
         data = decoder.decode(raw_record)
         # print("Decoded file :", os.path.join(tfrecord_path, each_file))
         image = data['image']
-        assert len(image.shape) == 3
+        contigious_mask = tf.cast(data['groundtruth_panoptic_contigious_mask'][:, :, 0],
+            dtype=tf.float32)
+        instance_mask = tf.cast(
+            data['groundtruth_panoptic_instance_mask'][:, :, 0],
+            dtype=tf.float32)
+        h,w,c = image.shape
+        assert len(image.shape) == 3 
         assert image.shape[-1] == 3
         assert image.numpy().all() <= 255
+        assert image.numpy().all() >= 0
+        assert image.numpy().shape[0] >= 0
+        assert image.numpy().shape[1] >= 0
+
+        assert len(contigious_mask.shape) == 2
+        assert contigious_mask.numpy().all() <= 132
+        assert contigious_mask.numpy().all() >= 0
+        assert contigious_mask.numpy().shape[0] >= 0
+        assert contigious_mask.numpy().shape[1] >= 0
+        assert contigious_mask.numpy().shape[0] == h
+        assert contigious_mask.numpy().shape[1] == w
+
+        assert len(instance_mask.shape) == 2
+        assert instance_mask.numpy().all() <= 132
+        assert instance_mask.numpy().all() >= 0
+        assert instance_mask.numpy().shape[0] >= 0
+        assert instance_mask.numpy().shape[1] >= 0
+        assert instance_mask.numpy().shape[0] == h
+        assert instance_mask.numpy().shape[1] == w
+
         image_count += 1
     
 print("Total images :", image_count)
