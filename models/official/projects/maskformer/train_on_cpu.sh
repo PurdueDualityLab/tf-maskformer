b2de93aedd1a72404c14c32aa56bc63d15ac5432
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-17 09:47:17 -0400

    updtaed code

--- models/official/projects/maskformer/train_on_cpu.sh
+++ models/official/projects/maskformer/train_on_cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-train_bsize=2
-eval_bsize=2
+train_bsize=1
+eval_bsize=1
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
 export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP01_CPU"
@@ -12,10 +12,10 @@ export IMG_SIZE=1280
 export PRINT_OUTPUTS=True
 export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precision_dtype=float32,\
 task.train_data.global_batch_size=$TRAIN_BATCH_SIZE,\
-task.model.which_pixel_decoder=transformer_fpn,\
-task.init_checkpoint=$RESNET_CKPT"
+task.model.which_pixel_decoder=transformer_fpn"
+# task.init_checkpoint=$RESNET_CKPT"
 python3 models/official/projects/maskformer/train.py \
   --experiment maskformer_coco_panoptic \
-  --mode train_and_eval \
+  --mode train \
   --model_dir $MODEL_DIR \
   --params_override=$OVERRIDES
