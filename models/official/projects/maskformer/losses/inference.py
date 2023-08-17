f5377d69ddf5ef519d742ef26e991f9b2163108f
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-17 13:59:06 -0400

    added postprocessing logic

--- models/official/projects/maskformer/losses/inference.py
+++ models/official/projects/maskformer/losses/inference.py
@@ -23,6 +23,7 @@ class PanopticInference:
         
         # Keep only those masks that have high confidence and are not background
         # TODO : Some operations may be incompatible on TPU
+        print("Scores : ", scores)
         print("Labels : ", labels)
         print("Background class id", self.background_class_id)
         keep = tf.math.logical_and(tf.math.not_equal(labels, self.background_class_id), scores > self.object_mask_threshold)
