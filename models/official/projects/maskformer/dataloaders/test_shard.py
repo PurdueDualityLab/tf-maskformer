47999358f4fa9af5cb9fa9be652349b40fdbda90
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-17 02:51:09 -0400

    verifying all the tfrecords

--- models/official/projects/maskformer/dataloaders/test_shard.py
+++ models/official/projects/maskformer/dataloaders/test_shard.py
@@ -11,11 +11,17 @@ tfrecord_path = "/home/vishalpurohit55595/datasets/coco_panoptic/tfrecords"  # s
 # get list of tfrecord files
 file_paths = tf.io.gfile.glob(tfrecord_path + "/*.tfrecord")
 decoder = panoptic_input.TfExampleDecoder()
+image_count = 0
 for each_file in file_paths:
     raw_dataset = tf.data.TFRecordDataset(os.path.join(tfrecord_path, each_file))
     print("Reading file :", os.path.join(tfrecord_path, each_file))
     for raw_record in raw_dataset.take(-1):
         data = decoder.decode(raw_record)
-        print("Decoded file :", os.path.join(tfrecord_path, each_file))
+        # print("Decoded file :", os.path.join(tfrecord_path, each_file))
         image = data['image']
-        print("Image shape :", image.shape)
+        assert len(image.shape) == 3
+        assert image.shape[-1] == 3
+        assert image <= 255
+        image_count += 1
+    
+print("Total images :", image_count)
