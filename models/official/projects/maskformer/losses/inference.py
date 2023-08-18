16668d9895fe17051a9de3e04dc7566e89f7640b
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-17 22:52:04 -0400

    testing inference

--- models/official/projects/maskformer/losses/inference.py
+++ models/official/projects/maskformer/losses/inference.py
@@ -23,12 +23,12 @@ class PanopticInference:
         
         # Keep only those masks that have high confidence and are not background
         # TODO : Some operations may be incompatible on TPU
-        print("Scores : ", scores)
-        print("Labels : ", labels)
-        print("Background class id", self.background_class_id)
+        # print("Scores : ", scores)
+        # print("Labels : ", labels)
+        # print("Background class id", self.background_class_id)
         keep = tf.math.logical_and(tf.math.not_equal(labels, self.background_class_id), scores > self.object_mask_threshold)
-        print("keep", keep)
-        exit()
+        # print("keep", keep)
+        # exit()
         curr_scores = scores[keep]
         curr_classes = labels[keep]
 
