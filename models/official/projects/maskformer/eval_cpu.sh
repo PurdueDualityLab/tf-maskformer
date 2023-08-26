7d6851878c9b6e0ef853fdf7f96b17f930c57d39
Vishal-S-P <vishal_purohit@outlook.com>
2023-08-26 19:32:36 -0400

    updated code for eval on CPU

--- models/official/projects/maskformer/eval_cpu.sh
+++ models/official/projects/maskformer/eval_cpu.sh
@@ -3,13 +3,14 @@ train_bsize=2
 eval_bsize=2
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP13_v8_eval"
-export MASKFORMER_CKPT="gs://cam2-models/maskformer_vishal_exps/EXP13_v8/ckpt-1848"
+export MASKFORMER_CKPT="gs://cam2-models/maskformer_vishal_exps/EXP13_v8/ckpt-672672"
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
 export TFRECORDS_DIR="gs://cam2-datasets/coco_panoptic/tfrecords"
 export TRAIN_BATCH_SIZE=$train_bsize
 export EVAL_BATCH_SIZE=$eval_bsize
 export BASE_LR=0.0001
-export IMG_SIZE=640
+export IMG_SIZE=1280
+export PRINT_OUTPUTS=True
 export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precision_dtype=float32,\
 task.validation_data.global_batch_size=$EVAL_BATCH_SIZE,task.model.which_pixel_decoder=transformer_fpn,\
 task.init_checkpoint_modules=all,\
