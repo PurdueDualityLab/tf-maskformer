7580320de7b93fbe361e93594536e45202e312b5
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-02 17:55:05 -0400

    exp15_v3 - changed eval batch size to fit into memory

--- models/official/projects/maskformer/exp015_v8.sh
+++ models/official/projects/maskformer/exp015_v8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 train_bsize=64
-eval_bsize=64
+eval_bsize=8
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
 export MODEL_DIR="gs://cam2-models/maskformer_vishal_exps/EXP15_v8"
