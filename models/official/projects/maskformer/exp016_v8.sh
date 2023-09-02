eb84192cba05c4943d0a349f5f57c3539627fe66
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-02 17:40:53 -0400

    exp16_v3

--- models/official/projects/maskformer/exp016_v8.sh
+++ models/official/projects/maskformer/exp016_v8.sh
@@ -12,6 +12,7 @@ export TFRECORDS_DIR="gs://cam2-datasets/coco_panoptic/tfrecords"
 export TRAIN_BATCH_SIZE=$train_bsize
 export EVAL_BATCH_SIZE=$eval_bsize
 export BASE_LR=0.0001
+export NO_OBJ_CLS_WEIGHT=0.01
 export IMG_SIZE=640
 export PRINT_OUTPUTS=False
 export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=float32,\
