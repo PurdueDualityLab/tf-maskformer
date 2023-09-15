bc2e0cef83de77b764030af2177c8d88acac4994
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-15 16:25:34 -0400

    update

--- models/official/projects/detr/train_exp2.sh
+++ models/official/projects/detr/train_exp2.sh
@@ -11,7 +11,7 @@ export TPU_SOFTWARE="2.12.0"
 export TPU_PROJECT="red-atlas-305317"
 export TPU_ZONE="us-central1-a"
 export CONFIG_FILE="models/official/projects/detr/configs/detr_tpu_v3_640.yaml"
-# export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=float32,\
+export OVERRIDES="runtime.distribution_strategy=tpu,runtime.mixed_precision_dtype=float32"
 # task.validation_data.global_batch_size=2,task.model.which_pixel_decoder=transformer_fpn,\
 # task.init_checkpoint_modules=all,\
 # task.init_checkpoint=$MODEL_DIR"
