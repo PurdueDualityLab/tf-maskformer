dec72f42f278072b0672f53eb38ac95c0166ed6b
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-16 15:16:45 -0400

    update

--- models/official/projects/detr/exp01_cpu.sh
+++ models/official/projects/detr/exp01_cpu.sh
@@ -4,6 +4,7 @@ eval_bsize=2
 export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export MODEL_DIR="gs://cam2-models/detr_vishal_exps/EXP01_cpu"
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
+export TFRECORDS_DIR="gs://cam2-datasets/coco"
 export TPU_NAME="tf-debug-1"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
@@ -12,8 +13,10 @@ export TRAIN_BATCH_SIZE=$train_bsize
 export EVAL_BATCH_SIZE=$eval_bsize
 export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precision_dtype=float32,\
 task.train_data.global_batch_size=$TRAIN_BATCH_SIZE,\
-task.init_checkpoint=$RESNET_CKPT"
-export CONFIG_FILE="models/official/projects/detr/configs/detr_tpu_v3_640_train_cpu.yaml"
+task.init_checkpoint=$RESNET_CKPT,\
+task.train_data.input_path=$TFRECORDS_DIR,\
+task.validation_data.input_path=$TFRECORDS_DIR"
+# export CONFIG_FILE="models/official/projects/detr/configs/detr_tpu_v3_640_train_cpu.yaml"
 python3 models/official/projects/detr/train.py \
   --experiment detr_coco_tfrecord \
   --mode train_and_eval \
