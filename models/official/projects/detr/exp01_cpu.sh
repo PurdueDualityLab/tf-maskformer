8c31697e9cfd1a209b113732bdb6d7add419b1fc
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-16 15:19:02 -0400

    update

--- models/official/projects/detr/exp01_cpu.sh
+++ models/official/projects/detr/exp01_cpu.sh
@@ -5,6 +5,7 @@ export PYTHONPATH=$PYTHONPATH:~/tf-maskformer/models
 export MODEL_DIR="gs://cam2-models/detr_vishal_exps/EXP01_cpu"
 export RESNET_CKPT="gs://cam2-models/maskformer_vishal_exps/resnet50_pretrained/tfmg/ckpt-62400"
 export TFRECORDS_DIR="gs://cam2-datasets/coco"
+export ANNOTATION_FILE="gs://cam2-datasets/annotations/instances_val2017.json"
 export TPU_NAME="tf-debug-1"
 export TPU_SOFTWARE="2.11.0"
 export TPU_PROJECT="red-atlas-305317"
@@ -15,7 +16,8 @@ export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precisi
 task.train_data.global_batch_size=$TRAIN_BATCH_SIZE,\
 task.init_checkpoint=$RESNET_CKPT,\
 task.train_data.input_path=$TFRECORDS_DIR,\
-task.validation_data.input_path=$TFRECORDS_DIR"
+task.validation_data.input_path=$TFRECORDS_DIR,\
+task.annotation_file=$ANNOTATION_FILE"
 # export CONFIG_FILE="models/official/projects/detr/configs/detr_tpu_v3_640_train_cpu.yaml"
 python3 models/official/projects/detr/train.py \
   --experiment detr_coco_tfrecord \
