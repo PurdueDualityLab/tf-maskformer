4af85d90bd70f5939b90d02315ae88c0d740af03
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-17 02:10:30 -0400

    update

--- models/official/projects/maskformer/tasks/panoptic_maskformer.py
+++ models/official/projects/maskformer/tasks/panoptic_maskformer.py
@@ -73,7 +73,7 @@ class PanopticTask(base_task.Task):
 		if self._task_config.init_checkpoint_modules == 'all':
 			ckpt = tf.train.Checkpoint(**model.checkpoint_items)
 			status = ckpt.restore(ckpt_dir_or_file)
-			status.expect_partial().assert_existing_objects_matched()
+			status.assert_consumed()
 			logging.info('Loaded whole model from %s',ckpt_dir_or_file)
 			
 		elif self._task_config.init_checkpoint_modules == 'backbone':
