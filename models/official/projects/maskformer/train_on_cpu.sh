40b1fdb992cda035f8d5f40ce65ed9d403072d40
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-17 02:43:56 -0400

    update

--- models/official/projects/maskformer/train_on_cpu.sh
+++ models/official/projects/maskformer/train_on_cpu.sh
@@ -8,14 +8,15 @@ export TFRECORDS_DIR="gs://cam2-datasets/coco_panoptic/tfrecords"
 export TRAIN_BATCH_SIZE=$train_bsize
 export EVAL_BATCH_SIZE=$eval_bsize
 export BASE_LR=0.0001
-export IMG_SIZE=1333
+export IMG_SIZE=640
+export NO_OBJ_CLS_WEIGHT=0.1
 export PRINT_OUTPUTS=True
-# export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precision_dtype=float32,\
-# task.train_data.global_batch_size=$TRAIN_BATCH_SIZE,\
-# task.model.which_pixel_decoder=transformer_fpn,\
-# task.init_checkpoint=$RESNET_CKPT"
+export OVERRIDES="runtime.distribution_strategy=one_device,runtime.mixed_precision_dtype=float32,\
+task.train_data.global_batch_size=$TRAIN_BATCH_SIZE,\
+task.model.which_pixel_decoder=transformer_fpn,\
+task.init_checkpoint=$RESNET_CKPT"
 python3 models/official/projects/maskformer/train.py \
   --experiment maskformer_coco_panoptic \
-  --mode train_and_eval \
+  --mode train \
   --model_dir $MODEL_DIR \
   --params_override=$OVERRIDES
