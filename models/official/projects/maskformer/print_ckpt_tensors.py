42ca1db1eff85a6459a9efe4c64ff9641bcc3e78
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-19 09:57:41 -0400

    updated

--- models/official/projects/maskformer/print_ckpt_tensors.py
+++ models/official/projects/maskformer/print_ckpt_tensors.py
@@ -1,3 +1,4 @@
 from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
+import tensorflow as tf
 latest_ckp = tf.train.latest_checkpoint('gs://cam2-models/maskformer_vishal_exps/EXP19_v8')
 print_tensors_in_checkpoint_file(latest_ckp, all_tensors=True, tensor_name='')
