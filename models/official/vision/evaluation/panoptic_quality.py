59b8b1af7e63145d9d61c960d625f5cd352ab961
Vishal-S-P <vishal_purohit@outlook.com>
2023-09-27 17:15:11 -0400

    updated code

--- models/official/vision/evaluation/panoptic_quality.py
+++ models/official/vision/evaluation/panoptic_quality.py
@@ -425,10 +425,11 @@ class PanopticQualityV2(tf.keras.metrics.Metric):
     gt_instance_mask = tf.cast(y_true['instance_mask'], tf.int32)
 
     # print shapes of input tensors
-    print("category_mask shape :", category_mask.shape)
-    print("instance_mask shape :", instance_mask.shape)
-    print("gt_category_mask shape :", gt_category_mask.shape)
-    print("gt_instance_mask shape :", gt_instance_mask.shape)
+    tf.print("category_mask shape :", category_mask.shape)
+    tf.print("instance_mask shape :", instance_mask.shape)
+    tf.print("gt_category_mask shape :", gt_category_mask.shape)
+    tf.print("gt_instance_mask shape :", gt_instance_mask.shape)
+    exit()
     if self._rescale_predictions:
       _, height, width = gt_category_mask.get_shape().as_list()
       # Instead of cropping the masks to the original image shape (dynamic),
