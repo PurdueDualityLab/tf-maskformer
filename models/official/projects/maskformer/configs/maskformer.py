6849b1d3d40d8a645cdfae85a1b620f97def15df
Vishal-S-P <vishal_purohit@outlook.com>
2023-10-13 17:39:05 -0400

    removed max_keep = 3

--- models/official/projects/maskformer/configs/maskformer.py
+++ models/official/projects/maskformer/configs/maskformer.py
@@ -183,8 +183,7 @@ def maskformer_coco_panoptic() -> cfg.ExperimentConfig:
           steps_per_loop=steps_per_epoch,
           summary_interval=steps_per_epoch,
           checkpoint_interval=steps_per_epoch,
-          validation_interval=steps_per_epoch, # run validation after every epoch (not efficient, but we want to see the results)
-          max_to_keep=3,
+          validation_interval=steps_per_epoch, # run validation after every epoch (not efficient, but we want to see the results)sss
           best_checkpoint_export_subdir='best_ckpt',
           # TODO: Metric not implemented yet
           optimizer_config=optimization.OptimizationConfig({
